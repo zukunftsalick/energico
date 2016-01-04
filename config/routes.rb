5f4d9b2d8222f681969ac59fbaf3e603a9e575c8
Pedro Pimentel <zukunft@gmail.com>
2016-01-04 11:19:01 +0800

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 	get 'forex/hang_seng', to: 'forex#hang_seng'
+
+	root 'forex#hang_seng'
 end
